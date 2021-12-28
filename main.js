012b2f4684320c4bc39a1e6cc7c2c46b6bd6f1d3
mug-coder <81393910+mug-coder@users.noreply.github.com>
2021-12-28 10:25:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song="";
+song_2="";
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+
+
